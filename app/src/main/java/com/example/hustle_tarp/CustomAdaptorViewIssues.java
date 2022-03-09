6bb9c4d9b7617e296d9bf4b4b0e842398b75c47c
sonashubhamvats <sona.svats23@gmail.com>
2022-03-09 16:11:19 +0530

    first_commit

---
+++ app/src/main/java/com/example/hustle_tarp/CustomAdaptorViewIssues.java
@@ -0,0 +1,55 @@
+package com.example.hustle_tarp;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
+
+import java.util.ArrayList;
+
+public class CustomAdaptorViewIssues extends ArrayAdapter<Issues> {
+
+    public CustomAdaptorViewIssues(@NonNull Context context, ArrayList<Issues> arrayList) {
+        super(context, 0, arrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View currentItemView = convertView;
+
+
+
+        if (currentItemView == null) {
+            currentItemView = LayoutInflater.from(getContext()).inflate(R.layout.custom_viewissues_list_view, parent, false);
+        }
+
+        try{
+            Issues issue = getItem(position);
+
+            TextView title = currentItemView.findViewById(R.id.title_custom_listView);
+            TextView desc = currentItemView.findViewById(R.id.description_custom_listView);
+            TextView credits=currentItemView.findViewById(R.id.credits_custom_ListView);
+            TextView due_date=currentItemView.findViewById(R.id.dueDate_listView);
+
+            title.setText(issue.getTitle());
+            desc.setText(issue.getDescription());
+            credits.setText(issue.getCredits());
+            due_date.setText(issue.getDue_date());
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        return currentItemView;
+    }
+}
---
+++ app/src/main/java/com/example/hustle_tarp/ViewExpandedIssueEmployee.java
@@ -0,0 +1,37 @@
+package com.example.hustle_tarp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ViewExpandedIssueEmployee extends AppCompatActivity {
+    String title,desc,credits,link,duedate,issue_id;
+    TextView title_expandedViewIssuesEmployee,desc_expandedViewIssuesEmployee,credits_expandedViewIssuesEmployee,
+    link_expandedViewIssuesEmployee,duedate_expandedViewIssuesEmployee;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_expanded_issue_employee);
+        Intent i=getIntent();
+        title=i.getStringExtra("title");
+        desc=i.getStringExtra("desc");
+        credits=i.getStringExtra("credits");
+        link=i.getStringExtra("link");
+        duedate=i.getStringExtra("duedate");
+        issue_id=i.getStringExtra("issueid");
+        title_expandedViewIssuesEmployee=findViewById(R.id.title_employee_expanded_view);
+        desc_expandedViewIssuesEmployee=findViewById(R.id.desc_employee_expanded_view);
+        credits_expandedViewIssuesEmployee=findViewById(R.id.credits_employee_expanded_view);
+        link_expandedViewIssuesEmployee=findViewById(R.id.link_employee_expanded_view);
+        duedate_expandedViewIssuesEmployee=findViewById(R.id.duedate_employee_expanded_view);
+        title_expandedViewIssuesEmployee.setText(title);
+        desc_expandedViewIssuesEmployee.setText(desc);
+        credits_expandedViewIssuesEmployee.setText(credits);
+        link_expandedViewIssuesEmployee.setText(link);
+        duedate_expandedViewIssuesEmployee.setText(duedate);
+        Toast.makeText(this, ""+issue_id, Toast.LENGTH_SHORT).show();
+    }
+}
