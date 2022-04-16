1d7eb797362b6e3494c296f79c958595a81583e5
sonashubhamvats <sona.svats23@gmail.com>
2022-04-16 20:24:36 +0530

    added_the_notifications_and_deletion of issues

--- app/src/main/java/com/example/hustle_tarp/employeeLandingPage.java
+++ app/src/main/java/com/example/hustle_tarp/employeeLandingPage.java
@@ -6,11 +6,13 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,7 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class employeeLandingPage extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater=getMenuInflater();
-        menuInflater.inflate(R.menu.menulogout,menu);
+        menuInflater.inflate(R.menu.menulogoutnotifications,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -28,16 +30,22 @@ public class employeeLandingPage extends AppCompatActivity {
         super.onOptionsItemSelected(item);
         switch(item.getItemId())
         {
-            case R.id.logout:
+            case R.id.logoutEmp:
                 FirebaseAuth.getInstance().signOut();
                 finish();
                 startActivity(new Intent(this,LoginPage.class));
                 return  true;
-
+            case R.id.notificationEmp:
+                showNotifications();
+                return true;
             default:
                 return false;
         }
 
+    }
+    void showNotifications()
+    {
+        Toast.makeText(this, "NotificationsShowed", Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
